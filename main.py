ac0520d01924e74f4c6eeb1f40bd6dcf1605c8a1
adamtomydas <adamtomydas@gmail.com>
2024-04-02 18:56:27 -0500

    MySQL Integration

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World!")
+import mysql.connector
+
+connection = mysql.connector.connect(user = 'root', database = 'example', password = 'password')
+
+cursor = connection.cursor()
+
+testQuery = ("SELECT * FROM banking")
+cursor.execute(testQuery)
+
+for item in cursor:
+    print(item)
+
+cursor.close()
+connection.close()
